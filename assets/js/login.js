a1d2e9049f68cdbdcb3de4aa231133a29b53d958
chenyang <1055382780@qq.com>
2021-01-04 15:40:18 +0800

    完成了主页得开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -35,55 +35,46 @@ if(pwd !==value ){
     })
     // 校验两次密码是否一致
 // 监听注册表单的提交事件
-$('#form-reg').on('submit',function(e){
-e.preventDefault()
-$.ajax({
-method:'post',
-url:'/api/reguser',
-data:{
-    username:$('#reg-name').val(),
-    password:$('#reg-pw').val()
-},
-success:function(res){
-    if(res.status!==0){
-      return layer.msg(res.message);
-    }else{
-        layer.msg('注册成功，请登录！');
-
-        $('#link-login').click()
-    } 
-
-}
-
-
-})
-
-})
-
-// 监听登录表单的提交事件
-$('#form-login').on('submit',function(e){
-    // 组织默认提交
+ // 监听注册表单的提交事件
+ $('#form-reg').on('submit', function(e) {
+    // 1. 阻止默认的提交行为
     e.preventDefault()
-$.ajax({
-    url:'/api/login',
-    method:'post',
-    // 快速获取表单数据
-    data:$(this).serialize(),
-   success:function(res){
-if(res.status!==0){
-    return layer.msg('登录失败');
-}else{
-    layer.msg('登录成功')
-    // localStorage.setItem('token',res.token)
-// console.log(res.token);
-    // 登录成功后跳转到后台
- location.href='/index.html'
-}
-}
+    // 2. 发起Ajax的POST请求
+    var data = {
+      username: $('#form-reg [name=username]').val(),
+      password: $('#form-reg [name=password]').val()
+    }
+    $.post('/api/reguser', data, function(res) {
+      if (res.status !== 0) {
+        return layer.msg(res.message)
+      }
+      layer.msg('注册成功，请登录！')
+      // 模拟人的点击行为
+      $('#link_login').click()
+    })
+  })
 
-})
-
-})
+  // 监听登录表单的提交事件
+  $('#form-login').submit(function(e) {
+    // 阻止默认提交行为
+    e.preventDefault()
+    $.ajax({
+      url: '/api/login',
+      method: 'POST',
+      // 快速获取表单中的数据
+      data: $(this).serialize(),
+      success: function(res) {
+        if (res.status !== 0) {
+          return layer.msg('登录失败！')
+        }
+        layer.msg('登录成功！')
+        // 将登录成功得到的 token 字符串，保存到 localStorage 中
+        localStorage.setItem('token', res.token)
+        // 跳转到后台主页
+        location.href = '/index.html'
+      }
+    })
+  })
 
 
 
